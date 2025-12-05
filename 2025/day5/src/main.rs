126c3f9a7b7020cf7b8f27a80f6adf4e5a326b0e
CEbbinghaus <git@cebbinghaus.com>
2025-12-05 18:17:43 +1100

    Optimized day 5 and added speed test

--- 2025/day5/src/main.rs
+++ 2025/day5/src/main.rs
@@ -1,4 +1,4 @@
-use std::ops::{RangeInclusive};
+use std::ops::RangeInclusive;
 
 fn parse_ranges(fresh_ranges: &Vec<&str>) -> Vec<RangeInclusive<usize>> {
     let mut ranges = Vec::new();
@@ -16,17 +16,19 @@ fn parse_ranges(fresh_ranges: &Vec<&str>) -> Vec<RangeInclusive<usize>> {
     ranges
 }
 
-fn solution1(fresh_ingredients: &Vec<&str>, ingredients: &Vec<&str>) -> usize {
-    let ranges = parse_ranges(fresh_ingredients);
-
+fn solution1(
+    fresh_ingredient_ranges: &Vec<RangeInclusive<usize>>,
+    ingredients: &Vec<&str>,
+) -> usize {
     let mut total = 0_usize;
     for ingredient in ingredients {
         let ingredient = ingredient.parse::<usize>().unwrap();
 
-        let mut iter = ranges.iter();
-        'inner: while let Some(range) = iter.next() && *range.start() <= ingredient
+        let mut iter = fresh_ingredient_ranges.iter();
+        'inner: while let Some(range) = iter.next()
+            && *range.start() <= ingredient
         {
-            if  range.contains(&ingredient) {
+            if range.contains(&ingredient) {
                 total += 1;
                 break 'inner;
             }
@@ -35,10 +37,14 @@ fn solution1(fresh_ingredients: &Vec<&str>, ingredients: &Vec<&str>) -> usize {
     total
 }
 
-fn solution2(fresh_ingredients: &Vec<&str>) -> usize {
-    let ranges = parse_ranges(fresh_ingredients);
+fn solution2(fresh_ingredient_ranges: &Vec<RangeInclusive<usize>>) -> usize {
+    fresh_ingredient_ranges
+        .iter()
+        .map(|r| r.end() - (r.start() - 1))
+        .sum()
+}
 
-    // Merge overlapping ranges
+fn merge_ranges(ranges: &Vec<RangeInclusive<usize>>) -> Vec<RangeInclusive<usize>> {
     let mut merged_ranges: Vec<RangeInclusive<usize>> = Vec::new();
     for range in ranges {
         // TIL you can get a mutable reference to the last element of a vector
@@ -49,10 +55,9 @@ fn solution2(fresh_ingredients: &Vec<&str>) -> usize {
                 continue;
             }
         }
-        merged_ranges.push(range);
+        merged_ranges.push(range.clone());
     }
-
-    merged_ranges.iter().map(|r| r.end() - (r.start() - 1)).sum()
+    merged_ranges
 }
 
 fn main() {
@@ -65,16 +70,20 @@ fn main() {
     let (fresh_ingredients, ingredients) =
         contents.split_once("\n\n").expect("could not split input");
 
-    let fresh_ingredients: Vec<&str> = fresh_ingredients.lines().collect();
     let ingredients: Vec<&str> = ingredients.lines().collect();
+    let fresh_ingredients = parse_ranges(&fresh_ingredients.lines().collect());
+    let fresh_ingredients = merge_ranges(&fresh_ingredients);
 
+    let start = std::time::Instant::now();
     println!(
         "Solution 1: {}",
         solution1(&fresh_ingredients, &ingredients)
     );
+    let duration = start.elapsed();
+    println!("Time elapsed in solution 1() is: {:?}", duration);
 
-    println!(
-        "Solution 2: {}",
-        solution2(&fresh_ingredients)
-    );
+    let start = std::time::Instant::now();
+    println!("Solution 2: {}", solution2(&fresh_ingredients));
+    let duration = start.elapsed();
+    println!("Time elapsed in solution 2() is: {:?}", duration);
 }
